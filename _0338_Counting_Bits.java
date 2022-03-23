f3b5b59946e89b527722a25131507d76ee3e476c
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-03-23 08:44:37 +0530

    Updated solution for _0338_Counting_Bits in O(n)

--- _0338_Counting_Bits.java
+++ _0338_Counting_Bits.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/counting-bits/
+
 import java.util.Arrays;
 
 public class _0338_Counting_Bits
@@ -7,7 +9,31 @@ public class _0338_Counting_Bits
         System.out.println(Arrays.toString(countBits(iNo)));
     }
 
+
+
+    // UPDATED Solution O(n)
     public static int[] countBits(int iNo)
+    {
+        int[] ans  = new int[iNo+1];
+        ans[0] = 0;
+
+        for(int i = 1; i <= iNo; i++)
+        {
+            if((i & 1) == 0)
+            {
+                ans[i] = ans[i>>1];
+            }
+            else
+            {
+                ans[i] = ans[i>>1]+1;
+            }
+        }
+        return ans;
+    }
+
+
+    // PREVIOUS Solution O(n^2)
+    public static int[] oldCountBits(int iNo)
     {
         int[] ans  = new int[iNo+1];
         int bin;
