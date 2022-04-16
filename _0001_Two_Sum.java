7aadd01cd0b71a1c40aa3f839ac1a2c25a91fb65
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-04-16 19:30:31 +0530

    Optimised  _0001_Two_Sum Question
    O(N) Time Complexity
     Using HashMap

--- _0001_Two_Sum.java
+++ _0001_Two_Sum.java
@@ -1,12 +1,18 @@
 // https://leetcode.com/problems/two-sum/
 
+
+// Optimised  O(N) Time Complexity
+// HashMap Used
+
+
 public class _0001_Two_Sum {
     public static void main(String[] args) 
     {
         int arr[] = {-1,-2,-3,-4,-5};
         int iTarget = -8;
 
-        int ans[] = twoSum(arr,iTarget);
+        // int ans[] = twoSum(arr,iTarget);
+        int ans[] = twoSumOptimised(arr,iTarget);
         for(int i = 0; i < ans.length; i++)
         {
             System.out.print(ans[i]+"\t");
@@ -14,6 +20,28 @@ public class _0001_Two_Sum {
 
         
         
+    }
+
+    // Optimised Function 
+    public static int[] twoSumOptimised(int[] arr, int target) {
+        
+        int[] ans  = {-1, -1};
+        HashMap<Integer, Integer> distinctElements = new HashMap<>(); 
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(distinctElements.containsKey(target - arr[i]))
+            {
+                ans[0] = i;
+                ans[1] = distinctElements.get(target - arr[i]);
+                break;
+            }
+            else
+            {
+                distinctElements.put(arr[i], i);
+            }
+        }
+        return ans;
     }
 
     static int[] twoSum(int arr[], int iTarget)
