0eab2ebcaac0d97ef5e0e9cfbe41b4ad4b231b78
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-04-24 00:27:32 +0530

    Optmised _0070_Climbing_Stairs.java with DP

--- _0070_Climbing_Stairs.java
+++ _0070_Climbing_Stairs.java
@@ -1,17 +1,52 @@
 // https://leetcode.com/problems/climbing-stairs/
 
+// Optimised with Dynamic Programming 
 
+import java.util.*;
 public class _0070_Climbing_Stairs {
     public static void main(String[] args) 
     {
         Solution obj = new Solution();
         int n = 45;
-        System.out.println(obj.climbStairs(n));    
+        System.out.println(obj.climbStairsOptimised(n));    
     }
 }
 
 class Solution {
-    public int climbStairs(int n)
+
+    int[] arr = new int[46];
+    public int climbStairsOptimised(int n)
+    {
+        Arrays.fill(arr,-1);
+        return climb(0,n);
+    }
+    public int climb(int current, int top)
+    {
+        if(current == top)
+        {
+            return 1;
+        }
+        
+        if(current > top)
+        {
+            return 0;
+        }
+        
+        // check in cheche
+        if(arr[current] != -1)
+        {
+            return arr[current];
+        }
+        
+        int oneStep = climb(current+1, top);
+        int twoStep = climb(current+2, top);
+        arr[current] = oneStep+twoStep;
+        return oneStep+twoStep;
+    }
+
+
+
+    public int climbStairsOld(int n)
     {
         if(n == 1)
         {
