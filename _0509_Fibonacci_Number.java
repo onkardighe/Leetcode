763561a69cc1e48d783f2e68fb39faf4c70783f2
Onkar Dighe <72162692+onkardighe@users.noreply.github.com>
2022-07-06 16:53:37 +0530

    Optimised with all types of approaches
    1. Recursion
    2. DP | Recursion + Memoisation
    3. DP | Tabulation
    4. DP | Storage Optimisation

--- _0509_Fibonacci_Number.java
+++ _0509_Fibonacci_Number.java
@@ -5,24 +5,94 @@ public class _0509_Fibonacci_Number
 {
     public static void main(String[] args) {
         int n = 5;
-        Solution obj = new Solution();
+        Solution0509 obj = new Solution0509();
         System.out.println(obj.fib(n));
+        System.out.println(obj.fibonacciMemoisation(n, new int[n+1]));
+        System.out.println(obj.fibonacciTabulation(n));
+        System.out.println(obj.fibonacciStorageOptimisation(n));
     }
     
 }
 
-class Solution {
+class Solution0509 {
     public int fib(int n) {
-        return fibonacci(n);
+        return fibonacciRecursion(n);
+    }
+
+    // DP By Storage Optimisation
+    // Time : O(n)
+    // Space : O(1)
+    public int fibonacciStorageOptimisation(int n)
+    {
+        
+        if(n <= 1)
+        {
+            return n;
+        }
+        
+        int prev1 = 1; 
+        int prev2 = 0;
+        
+        int curr = 0;
+        for(int i = 2; i <= n ; i++)
+        {
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+
+    // DP by Tabulation
+    // Time : O(n)
+    // Space : O(n)
+    public int fibonacciTabulation(int n)
+    {
+        
+        if(n <= 1)
+        {
+            return n;
+        }
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n ; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    // DP By Recusrion + Memoisation
+    // Time : O(n)
+    // Space : O(n)
+    public int fibonacciMemoisation(int n, int[] dp)
+    {
+        if(n <= 1)
+        {
+            return n;
+        }
+        
+        if(dp[n] != 0)
+        {
+            return dp[n];
+        }
+        
+        dp[n] =  fibonacciMemoisation(n-1, dp)+fibonacciMemoisation(n-2, dp);
+        return dp[n];
     }
     
-    public int fibonacci(int iNo)
+    // By Recursion
+    // Time : O(2n)
+    // Space : O(n)
+    public int fibonacciRecursion(int iNo)
     {
         if(iNo < 2)
         {
             return iNo;
         }
-        
-        return fibonacci(iNo-1) +  fibonacci(iNo-2);
+        return fibonacciRecursion(iNo-1) +  fibonacciRecursion(iNo-2);
     }
 }
